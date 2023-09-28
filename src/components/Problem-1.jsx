0b68ee1ad9ac849db3d7ce0bead97e9576928351
Li Pon <mirnatul@gmail.com>
2023-09-29 01:47:36 +0600

    task 1 completed

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,12 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const Problem1 = () => {
 
     const [show, setShow] = useState('all');
+    const [user, setUser] = useState('')
+    const [status, setStatus] = useState('')
 
-    const handleClick = (val) =>{
+    const [userStatus, setUserStatus] = useState([])
+
+    const handleClick = (val) => {
         setShow(val);
     }
+    console.log(show);
+
+    const activeUserStatus = userStatus.filter((each) => each.status === 'active')
+    const completedUserStatus = userStatus.filter((each) => each.status === 'completed')
+    // console.log(activeUserStatus);
+
+
+
+    const handleSubmit = (e) => {
+        // const allUser = []
+        e.preventDefault()
+        const userData = { name: user, status: status }
+        setUserStatus([...userStatus, userData])
+    }
+
+    // console.log(userStatus);
 
     return (
 
@@ -14,12 +34,12 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form onSubmit={handleSubmit} className="row gy-2 gx-3 align-items-center mb-4">
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input onChange={(e) => setUser(e.target.value)} type="text" className="form-control" placeholder="Name" />
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input onChange={(e) => setStatus(e.target.value)} type="text" className="form-control" placeholder="Status" />
                         </div>
                         <div className="col-auto">
                             <button type="submit" className="btn btn-primary">Submit</button>
@@ -29,25 +49,42 @@ const Problem1 = () => {
                 <div className="col-8">
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button className={`nav-link ${show === 'all' && 'active'}`} type="button" onClick={() => handleClick('all')}>All</button>
                         </li>
                         <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
+                            <button className={`nav-link ${show === 'active' && 'active'}`} type="button" onClick={() => handleClick('active')}>Active</button>
                         </li>
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button className={`nav-link ${show === 'completed' && 'active'}`} type="button" onClick={() => handleClick('completed')}>Completed</button>
                         </li>
                     </ul>
                     <div className="tab-content"></div>
                     <table className="table table-striped ">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
+                            {show === 'all' &&
+                                userStatus?.map((eachPair, index) => <tr key={index}>
+                                    <td>{eachPair.name}</td>
+                                    <td>{eachPair.status}</td>
+                                </tr>)
+                            }
+                            {show === 'active' &&
+                                activeUserStatus?.map((eachPair, index) => <tr key={index}>
+                                    <td>{eachPair.name}</td>
+                                    <td>{eachPair.status}</td>
+                                </tr>)
+                            }
+                            {show === 'completed' &&
+                                completedUserStatus?.map((eachPair, index) => <tr key={index}>
+                                    <td>{eachPair.name}</td>
+                                    <td>{eachPair.status}</td>
+                                </tr>)
+                            }
                         </tbody>
                     </table>
                 </div>
